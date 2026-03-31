b5ef092f5f933510aea47fdbb5586fe6b8780f9f
LucasLimaDeSouza <lukxx23121996@gmail.com>
2026-03-31 18:37:21 -0300

    feat: image component

---
+++ app/src/main/java/com/example/pratodoamorandroid/ui/components/ImageComponent.kt
@@ -0,0 +1,36 @@
+package com.example.pratodoamorandroid.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.pratodoamorandroid.R
+
+@Composable
+fun ImageComponent(
+    painterForImage: Painter = painterResource(R.drawable.heart)
+) {
+    Box(
+        modifier = Modifier.clip(RoundedCornerShape(26.dp)),
+
+    ) {
+        Image(
+            modifier = Modifier.size(64.dp),
+            contentDescription = "photo_or_image",
+            painter = painterForImage
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ImageComponentPreview() {
+    ImageComponent()
+}
